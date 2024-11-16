66e21a5d4c51111229c3b8a49b9b066a2c1206e6
ST10464207 <ST10464207@rcconnect.edu.za>
2024-11-16 20:25:52 +0000

    new comment

--- app/src/main/java/com/example/github/MainActivity.kt
+++ app/src/main/java/com/example/github/MainActivity.kt
@@ -5,17 +5,20 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import java.lang.reflect.Method
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        //we are now gonna see if its loaded
         }
     private fun NewMethod()
 
-    {
 
+{
+
+}
 
     }
     }
